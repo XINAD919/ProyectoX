b9601dabf9da053872f191f12d16868d83ca62c2
Daniel <Da_919@hotmail.com>
2020-10-26 17:02:24 -0500

    implemente el codigo de crear usuario y de logearse

--- index.py
+++ index.py
@@ -21,20 +21,45 @@ def home():
 @app.route('/login')
 def login():
   return render_template('login.html')
-##este es la prueba de conexion a la BD
-@app.route('/prueba', methods = ['POST'])
-def pruebaConexionBD():
+
+@app.route('/registrar')
+def registro():
+  return render_template('registro.html')
+
+#metodo para registrar usuario-----------------------------------------------------------------------
+@app.route('/registrarUsuario', methods = ['POST'])
+def registrarse():
   if request.method == 'POST':
     nombre = request.form['nombre']
     correo= request.form['correo']
+    contraseña= request.form['contraseña'] 
+    cur=mysql.connection.cursor()
+    cur.execute('INSERT INTO usuarios(Nombre,Correo,Contraseña)VALUES(%s,%s,SHA2(%s, 512))',
+                (nombre, correo, contraseña)) ## aqui se declara la sentencia SQL pero no se ejecuta
+    flash('el usuario fue creado exitosamente','success')
+    mysql.connection.commit() ### este ejecuta la sentencia SQL
+    return redirect(url_for('registro'))
+  #-------------------------------------------------------------------
+#metodo para iniciar sesion--------------------------------------------------------------------------
+@app.route('/iniciarSesion', methods = ['POST'])
+def iniciarSesion():
+  if request.method == 'POST':
+    correo= request.form['correo']
     contraseña= request.form['contraseña']
     cur=mysql.connection.cursor()
-    cur.execute('INSERT INTO usuarios(Nombre,Correo,Contraseña)VALUES(%s,%s,%s)',
-                (nombre, correo, contraseña)) ## aqui se declara la sentencia SQL pero no se ejecuta
-    flash('Registro exitoso')
-    mysql.connection.commit() ### este ejecuta la sentencia SQL
-    return redirect(url_for('home'))
-  
+    
+    cur.execute('SELECT Nombre, Correo FROM usuarios WHERE correo = %s AND contraseña = SHA2(%s, 512)',
+                (correo, contraseña))
+        
+    rs = cur.fetchone() #guarda lo que nos devuelve la ejecucion de la sentencia sql
+    
+    if rs == None:
+      flash('Algo no coincide con tu información','error')
+      return redirect(url_for('login'))
+    else:
+      flash('Bienvenido'+ ' '+ rs[0], 'success')
+      return redirect(url_for('home'))
+#--------------freeconk-----------------------------------------------------------
 @app.route('/Vistos-recientemente')
 def vistoReciente():
   pass
